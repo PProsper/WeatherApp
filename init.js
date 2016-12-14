a4921af1518469720ae7695c19b17f04eb099e1b
Mohammad Akhtar <mohammadakhtar@gmail.com>
2016-12-13 20:02:24 -0500

    Uploading more files from PC

---
+++ init.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var searchButton = document.querySelector('button');
+var userCity = document.querySelector('#city');
+var weatherDiv = document.querySelector('#weather');
+var weatherCity = document.querySelector('#weatherCity');
+var weatherDescription = document.querySelector('#weatherDescription');
+var weatherTemperature = document.querySelector('#weatherTemperature');
