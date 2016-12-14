85618f061f824276088c697895e3cd3ac09fb674
Mohammad Akhtar <mohammadakhtar@gmail.com>
2016-12-13 20:07:23 -0500

    Formatting changes

--- init.js
+++ init.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//setting some DOM references for use in app.js
+
 var searchButton = document.querySelector('button');
 var userCity = document.querySelector('#city');
 var weatherDiv = document.querySelector('#weather');
