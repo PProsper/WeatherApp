c5b0ea96974d987a3914f0d5f9a42b2ac2c7848f
Mohammad Akhtar <mohammadakhtar@gmail.com>
2016-12-13 20:01:46 -0500

    Delete init.js

--- init.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var searchButton = document.querySelector('button');
-var userCity = document.querySelector('#city');
-var weatherDiv = document.querySelector('#weather');
-var weatherCity = document.querySelector('#weatherCity');
-var weatherDescription = document.querySelector('#weatherDescription');
-var weatherTemperature = document.querySelector('#weatherTemperature');
